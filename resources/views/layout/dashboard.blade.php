0f7db35126cc69169d11f28f5c00a2b60d731d98
Shakeel Ahmad <shakeel2717@gmail.com>
2022-05-23 07:33:28 -0700

    Error Page Created

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -96,9 +96,6 @@
             @yield('content')
         </div>
     </div>
-    <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=[" your-google-map-api"]&libraries=places"></script>
     <script src="/assets/js/app.js"></script>
     <x-alert />
 </body>
