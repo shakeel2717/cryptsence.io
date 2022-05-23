de60469541471482b0204b2d1c4393283571dc4d
Shakeel Ahmad <shakeel2717@gmail.com>
2022-05-23 04:15:26 -0700

    make Clean Command Created

---
+++ app/Console/Commands/Clean.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class Clean extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:clean';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Seeding Database';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->call('migrate:fresh');
+        $this->call('cache:clear');
+        $this->call('config:clear');
+        $this->call('view:clear');
+        $this->call('route:clear');
+        $this->call('clear-compiled');
+        $this->call('optimize');
+
+
+        $user = new User();
+        $user->name = "Shakeel Ahmad";
+        $user->username = "shakeel2717";
+        $user->email = "shakeel2717@gmail.com";
+        $user->password = bcrypt('asdfasdf');
+        $user->save();
+
+
+
+        return 0;
+    }
+}
