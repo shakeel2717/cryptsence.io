7dfff9e2ca93a0476f33411b4974e19b64d5dae3
Shakeel Ahmad <shakeel2717@gmail.com>
2022-05-23 05:20:05 -0700

    migration update for login history

--- database/migrations/2022_05_23_120337_create_login_histories_table.php
+++ database/migrations/2022_05_23_120337_create_login_histories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('login_histories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('device')->nullable();
             $table->string('os')->nullable();
             $table->string('os_version')->nullable();
