971aa49aeeb4bd9472d4742713721eb092b0d278
raheelanwaar03 <raheelanwaar0316@gmail.com>
2022-05-28 13:35:19 +0500

    route deleted

--- routes/auth.php
+++ routes/auth.php
@@ -37,7 +37,6 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
                 ->name('verification.notice');
-    Route::view('/test','auth.verify-email');
 
     Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
                 ->middleware(['signed', 'throttle:6,1'])
