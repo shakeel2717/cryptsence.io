4963a30abf41472661ca9e5e9732762429d165b8
Shakeel Ahmad <shakeel2717@gmail.com>
2022-05-29 06:02:14 -0700

    Timezone update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('blockchain:run')
+        ->everyMinute()
+        ->emailOutputTo('shakeel2717@gmail.com');
+        ;
     }
 
     /**
@@ -25,7 +28,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
