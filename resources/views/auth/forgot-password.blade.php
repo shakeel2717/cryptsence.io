7fa688844057b2a56e9549b444602da2f90a5767
Shakeel Ahmad <shakeel2717@gmail.com>
2022-06-12 08:45:49 -0700

    User can Forgot Password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,36 +1,32 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
-
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('password.email') }}">
+@extends('layout.auth')
+@section('title')
+    Forgot Password
+@endsection
+@section('form')
+    <div class="my-auto mx-auto ms-xl-20 bg-white dark-bg-dark-1 bg-xl-transparent px-5 px-sm-8 py-8 p-xl-0 rounded-2 shadow-md shadow-xl-none w-full w-sm-3/4 w-lg-2/4 w-xl-auto">
+        <h2 class="intro-x fw-bold fs-2xl fs-xl-3xl text-center text-xl-start">
+            Forgot Password?
+        </h2>
+        <p class="intro-x mt-5 text-center text-xl-start">
+            Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
+        </p>
+        <form action="{{ route('password.email') }}" method="POST">
             @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <div class="intro-x mt-2 text-gray-500 d-xl-none text-center">Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.</div>
+            <div class="intro-x mt-8">
+                <input type="email" class="intro-x login__input form-control py-3 px-4 border-gray-300 d-block" name="email"
+                    placeholder="Email" value="{{ old('email') }}">
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
+            <div class="intro-x mt-5 mt-xl-8 text-center text-xl-start">
+                <button class="btn btn-primary py-3 px-4 w-full w-xl-32 me-xl-3 align-top">Submit</button>
+                <a href="{{ route('login') }}" class="btn btn-outline-secondary py-3 px-4 w-full w-xl-32 mt-3 mt-xl-0 align-top">Go Back</a>
+            </div>
+            <div class="intro-x mt-10 mt-xl-24 text-gray-700 dark-text-gray-600 text-center text-xl-start">
+                By signin in, you agree to our
+                <br>
+                <a class="text-theme-1 dark-text-theme-10" href="">Terms and Conditions</a> & <a
+                    class="text-theme-1 dark-text-theme-10" href="">Privacy Policy</a>
             </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+    </div>
+@endsection
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,48 +1,34 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('password.update') }}">
+@extends('layout.auth')
+@section('title')
+    Reset Password
+@endsection
+@section('form')
+    <div
+        class="my-auto mx-auto ms-xl-20 bg-white dark-bg-dark-1 bg-xl-transparent px-5 px-sm-8 py-8 p-xl-0 rounded-2 shadow-md shadow-xl-none w-full w-sm-3/4 w-lg-2/4 w-xl-auto">
+        <h2 class="intro-x fw-bold fs-2xl fs-xl-3xl text-center text-xl-start">
+            Reset your Password
+        </h2>
+        <form action="{{ route('password.update') }}" method="POST">
             @csrf
-
-            <!-- Password Reset Token -->
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+            <div class="intro-x mt-8">
+                <input type="email" class="intro-x login__input form-control py-3 px-4 border-gray-300 d-block" name="email"
+                    placeholder="Email" value="{{ old('email', $request->email) }}">
+                <input type="password" name="password"
+                    class="intro-x login__input form-control py-3 px-4 border-gray-300 d-block mt-4" placeholder="Password">
+                <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                <input type="password" name="password_confirmation"
+                    class="intro-x login__input form-control py-3 px-4 border-gray-300 d-block mt-4"
+                    placeholder="Confirm Password">
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required />
+            <div class="intro-x mt-5 mt-xl-8 text-center text-xl-start">
+                <button class="btn btn-primary py-3 px-4 w-full w-xl-32 me-xl-3 align-top">Reset</button>
             </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Reset Password') }}
-                </x-button>
+            <div class="intro-x mt-10 mt-xl-24 text-gray-700 dark-text-gray-600 text-center text-xl-start">
+                By signin in, you agree to our
+                <br>
+                <a class="text-theme-1 dark-text-theme-10" href="">Terms and Conditions</a> & <a
+                    class="text-theme-1 dark-text-theme-10" href="">Privacy Policy</a>
             </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+    </div>
+@endsection
