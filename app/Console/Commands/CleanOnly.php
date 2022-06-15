4c60474c28a67cc3b10f8a9f94a1c8b7155075c5
Shakeel Ahmad <shakeel2717@gmail.com>
2022-06-15 05:56:07 -0700

    cleanonly command for refer reward

--- app/Console/Commands/CleanOnly.php
+++ app/Console/Commands/CleanOnly.php
@@ -2,7 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Models\User;
+use App\Models\user\Transaction;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class CleanOnly extends Command
 {
@@ -31,8 +34,44 @@ class CleanOnly extends Command
         $this->call('view:clear');
         $this->call('route:clear');
         $this->call('config:clear');
-        $this->call('migrate');
         $this->call('schedule:clear-cache');
+
+        $users = User::all();
+        foreach ($users as $user) {
+            $reward = ReferralBalance($user->id);
+            if ($reward < 1) {
+                goto End;
+            }
+            Log::info('User: ' . $user->name . ' Referral Reward: ' . $reward);
+            // send this to stack
+            $transactionOut = new Transaction();
+            $transactionOut->user_id = $user->id;
+            $transactionOut->type = 'reward';
+            $transactionOut->coin_id = 2;
+            $transactionOut->amount = $reward;
+            $transactionOut->sum = 'out';
+            $transactionOut->status = 'approved';
+            $transactionOut->save();
+
+            Log::info('User: ' . $user->name . ' Referral Reward Removed: ' . $reward);
+            // adding new transaction
+
+            // adding balance into user account
+            $transaction = new Transaction();
+            $transaction->user_id = $user->id;
+            $transaction->amount = $reward;
+            $transaction->coin_id = 2;
+            $transaction->type = 'stack';
+            $transaction->status = 'approved';
+            $transaction->note = 'rewards to stack';
+            $transaction->sum = 'in';
+            $transaction->save();
+
+            Log::info('User: ' . $user->name . ' Referral Reward Added: ' . $reward);
+            End:
+        }
+
+
 
 
         return 0;
