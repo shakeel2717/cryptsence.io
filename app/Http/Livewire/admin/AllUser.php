70d91cec186cc75333867d35218cee3d6a805024
Shakeel Ahmad <shakeel2717@gmail.com>
2022-06-15 06:06:36 -0700

    all users with number format

--- app/Http/Livewire/admin/AllUser.php
+++ app/Http/Livewire/admin/AllUser.php
@@ -96,7 +96,7 @@ final class AllUser extends PowerGridComponent
                 return balanceIn('usdt.trc20', $model->id);
             })
             ->addColumn('reward', function (User $model) {
-                return ReferralBalance($model->id);
+                return number_format(ReferralBalance($model->id),2);
             })
             ->addColumn('refer')
             ->addColumn('created_at_formatted', fn (User $model) => Carbon::parse($model->created_at)->format('d/m/Y H:i:s'));
