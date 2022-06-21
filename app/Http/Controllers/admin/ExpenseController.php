e2e6a8ae4c540629cd96cb4a1be52c4392b98ff9
Shakeel Ahmad <shakeel2717@gmail.com>
2022-06-21 06:49:32 -0700

    expensses fixed

--- app/Http/Controllers/admin/ExpenseController.php
+++ app/Http/Controllers/admin/ExpenseController.php
@@ -15,7 +15,7 @@ class ExpenseController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.dashboard.expense.index');
     }
 
     /**
@@ -25,7 +25,7 @@ class ExpenseController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.dashboard.expense.create');
     }
 
     /**
