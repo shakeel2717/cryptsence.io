32384f9fba0fb67fa45365356c0d47bcb78693f8
Shakeel Ahmad <shakeel2717@gmail.com>
2022-08-09 19:10:09 +0500

    503 page update

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.error')
+@section('title')
+503
+@endsection
+@section('content')
+<div class="intro-x fs-8xl fw-medium">500</div>
+<div class="intro-x fs-xl fs-lg-3xl fw-medium mt-5">Please wait for a few minutes. this website will be Live after new updates installed.</div>
+<div class="intro-x fs-lg mt-3">We are already working on it, this Issue will be resolved soon.</div>
+<a href="{{ route('user.index.index') }}" class="intro-x btn py-3 px-4 text-white border-white dark-border-dark-5 dark-text-gray-300 mt-10">Back
+    to Home</a>
+@endsection
