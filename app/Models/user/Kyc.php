321a492754f0c545f4c0248a0b765f222773f96d
Shakeel Ahmed <79254929+shakeel2717@users.noreply.github.com>
2022-09-30 16:49:53 +0500

    Backup Configuration

---
+++ app/Models/user/Kyc.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\user;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kyc extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'phone',
+        'address',
+        'country',
+        'document',
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
